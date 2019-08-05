aaae879bfe36475c1f39263ccb177d7b5759d5ed
Francisco Javier Hernández <frahergal@users.noreply.github.com>
2019-08-05 12:49:21 +0200

    add js template literals

---
+++ js/template_literals.js
@@ -0,0 +1,8 @@
+// TEMPLATE LITERALS
+const name = "Javier";
+const profession = "Software engineer";
+// String concatenation with back-tick (`)
+console.log(`${name}, ${profession}\n`);
+// Use of templates inside strings
+const text = `Hi there!\nMy name is ${name} and I'm ${profession}`;
+console.log(text);
