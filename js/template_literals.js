3e724b90a9ee1d2ac2d5b87597e30fa64cfd24fc
Francisco Javier Hernández <frahergal@users.noreply.github.com>
2019-08-05 12:59:22 +0200

    add more cases to js template literals

--- js/template_literals.js
+++ js/template_literals.js
@@ -1,8 +1,18 @@
 // TEMPLATE LITERALS
+
 const name = "Javier";
 const profession = "Software engineer";
+
 // String concatenation with back-tick (`)
-console.log(`${name}, ${profession}\n`);
+console.log(`${name}, ${profession}\n`); // Explicit carriage return for code readability
+
 // Use of templates inside strings
-const text = `Hi there!\nMy name is ${name} and I'm ${profession}`;
+const text = `Hi there!
+My name is ${name} and I'm ${profession}`; // Implicit carriage return
 console.log(text);
+
+// Embedded expressions
+const price = 10;
+var discount = 0.1;
+console.log(`
+The product has a price of $${10 * (1 - 0.1)} after applying the discount.`);
